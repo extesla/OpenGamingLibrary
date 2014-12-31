4a1f90cf071b8d22ac84250cac89f0385201eb41
Sean Quinn <swquinn@gmail.com>
2014-12-31 01:01:12 -0500

    Adds the OpenGamingLibrary.Security project.

---
+++ Source/main/OpenGamingLibrary.Security/AuthenticationState.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2014 Extesla, LLC. All rights reserved.
+//
+// It is illegal to use, reproduce or distribute any part of this
+// Intellectual Property without prior written authorization from
+// Extesla, LLC.
+using System;
+
+namespace OpenGamingLibrary.Security
+{
+	public enum AuthenticationState
+	{
+		/// <summary>
+		/// The security context is not authenticated.
+		/// </summary>
+		NotAuthenticated,
+
+		/// <summary>
+		/// The security context is pending authentication.
+		/// </summary>
+		Pending,
+
+		/// <summary>
+		/// The security context is in the progress of authenticating a user.
+		/// </summary>
+		InProgress,
+
+		/// <summary>
+		/// The security context is fully authenticated.
+		/// </summary>
+		Authenticated
+	}
+}
+
